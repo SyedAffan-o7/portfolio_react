7a6540abbbfa85ad8f893b22d00ed0e04b5bfea0
SyedAffan-o7 <syedaffan.o88z@gmail.com>
2024-05-01 16:12:05 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import Header from "./Header";
+import HfirstS from "./Home Sections/H1S";
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <HfirstS />
+    </div>
+  );
+}
